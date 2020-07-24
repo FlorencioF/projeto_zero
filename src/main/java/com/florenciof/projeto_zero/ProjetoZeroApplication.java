c2baff5ca6ced2bb819ff390db1f321cfff66981
FlorencioF <fur_oliveira@hotmail.com>
2020-07-23 23:47:33 -0300

    primeiro commmit

---
+++ src/main/java/com/florenciof/projeto_zero/ProjetoZeroApplication.java
@@ -0,0 +1,13 @@
+package com.florenciof.projeto_zero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoZeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/florenciof/projeto_zero/ProjetoZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florenciof.projeto_zero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
